52a8061c6a420b134ffbf3a39c841a94a09cbf57
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2026-02-20 18:44:32 -0300

    feat: set default theme to dark in useTheme hook

--- frota_mirim_web/hooks/useTheme.tsx
+++ frota_mirim_web/hooks/useTheme.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 
 export function useTheme() {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", theme === "dark");
